068fe365d221718d535e96935859ab0cc5fcdb66
gandamanasibagaring <gandamanasibagarig@gmail.com>
2023-09-05 15:52:48 +0700

    h

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Halaman PHP</title>
+</head>
+<body>
+
+<h1>Selamat Datang di Halaman PHP</h1>
+
+<?php
+// Ini adalah contoh kode PHP
+$nama = "John Doe";
+echo "Halo, " . $nama . "! Selamat datang ke halaman PHP.";
+?>
+
+</body>
+</html>
+
